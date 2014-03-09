f3e6afdfa336adeca7bc9c92082e4f23343d9a9d
Rafael Bedia <dcat@trillinux.org>
2014-03-09 11:14:11 -0400

    Improved web UI.
    * Clicked IP now shows in a text field for easy copying.
    * IP address search box supports CIDR.searches (127.0.0.0/8) which zoom to an area that shows the prefix.
    * Refactored map.js to be easier to maintain.
    * Changed addLayer() to accept an argument for if it is a base layer. This allows the same function to be used for adding IP map layers and the label layer.

--- IPHeatmap/ipheatmap-server/src/main/webapp/index.jsp
+++ IPHeatmap/ipheatmap-server/src/main/webapp/index.jsp
@@ -1,44 +1,41 @@
 <%@page import="org.trillinux.ipheatmap.server.LayerRegistrar"%>
 <%@page import="org.trillinux.ipheatmap.server.Layer"%>
 <html>
-	<head>
-        <title>Gnutella2 Network IP Distribution Map</title>
-	    <script type="text/javascript" src="http://openlayers.org/api/OpenLayers.js"></script>
-	    <script type="text/javascript" src="hilbert.js"></script>
-	    <script type="text/javascript" src="ip.js"></script>
-	    <script type="text/javascript" src="map.js"></script>
-	    <script type="text/javascript">
-    	    var map;
+    <head>
+    <title>Gnutella2 Network IP Distribution Map</title>
+        <script type="text/javascript" src="http://openlayers.org/api/OpenLayers.js"></script>
+        <script type="text/javascript" src="hilbert.js"></script>
+        <script type="text/javascript" src="ip.js"></script>
+        <script type="text/javascript" src="map.js"></script>
+        <script type="text/javascript">
+        var map;
 
-		    function start() {
-		        map = createMap();
-		        <%
-		        for (Layer layer: LayerRegistrar.getInstance().getLayers()) {
-		        %>
-		        addLayer("<%=layer.getName()%>", "<%=layer.getPath()%>");
-		        <%    
-		        }
-		        %>
-                var labelLayer = new OpenLayers.Layer.XYZ('Labels', 'tile/labels/\${z}/\${x}/\${y}.png', {numZoomLevels: 10, alpha: true, layers: 'basic'});
-                labelLayer.setIsBaseLayer(false);       
-                map.addLayer(labelLayer);
-		        map.zoomToMaxExtent();
-		    }
-	    </script>
-	    <style type="text/css">
-	        /* avoid pink tiles */
-	        .olImageLoadError {
-	            background-color: transparent !important;
-	        }
-	    </style>
-	</head>
-    <body onload="start()">
+        function start() {
+            map = createMap();
+            <%
+            for (Layer layer : LayerRegistrar.getInstance().getLayers()) {
+            %>
+            addLayer("<%=layer.getName()%>", "<%=layer.getPath()%>", true);
+            <%
+            }
+            %>
+            addLayer('Labels', 'tile/labels/\${z}/\${x}/\${y}.png', false);
+            map.zoomToMaxExtent();
+        }
+        </script>
+        <style type="text/css">
+            /* avoid pink tiles */
+            .olImageLoadError {
+                background-color: transparent !important;
+            }
+        </style>
+    </head>
+    <body onload="start();">
         <div id="controls">
 			IP Address: <input id="ipField" type="text" value="<%= request.getRemoteAddr() %>">
-			<input id="search" type="button" value="Locate" onclick="searchClick()">
-			<span>Clicked location:</span> <span id="currentLocation"></span>
+			<input id="search" type="button" value="Locate" onclick="searchClick();">
+                        <span>Clicked location:</span> <input id="currentLocation" type="text" readonly="readonly">
 		</div>
 		<div style="width:100%; height:100%" id="map"></div>
     </body>
 </html>
-
--- IPHeatmap/ipheatmap-server/src/main/webapp/map.js
+++ IPHeatmap/ipheatmap-server/src/main/webapp/map.js
@@ -1,19 +1,37 @@
 
 function searchClick() {
-	var zoom = map.getZoom();
-	var scale = Math.pow(2, zoom - 1) * 256; // one edge length
-	var ip = document.getElementById('ipField').value;
-	var p = d2xy(Math.pow(2,32), dot2num(ip));
-	var x = p[0] / Math.pow(2,16) * scale;
-	var y = p[1] / Math.pow(2,16) * scale;
-	var lon = (x * 180 / scale) - 180;
-	var lat = -(y * 180 / scale - 90);
-	var lonlat = new OpenLayers.LonLat(lon, lat);
-	map.setCenter(lonlat, 6);
+    var searchText = document.getElementById('ipField').value;
+    var cidrRegexp = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\/(\d{1,2})/;
+    var match = cidrRegexp.exec(searchText);
+    if (match) {
+        var ip = match[1];
+        var lonlat = ip2lonlat(ip);
+        var cidr = +match[2]; // Convert string to number
+        var zoom = (cidr / 2) + 2;
+        map.setCenter(lonlat, zoom);
+    } else {
+        var ip = searchText;
+        var lonlat = ip2lonlat(ip);
+        map.setCenter(lonlat, 6);
+    }
 }
 
-function addLayer(name, path) {
-	map.addLayer(new OpenLayers.Layer.XYZ(name, path, {numZoomLevels: 10, alpha: true, layers: 'basic'}));
+function ip2lonlat(ip) {
+    var zoom = map.getZoom();
+    var scale = Math.pow(2, zoom - 1) * 256; // one edge length
+    var p = d2xy(Math.pow(2,32), dot2num(ip));
+    var x = p[0] / Math.pow(2,16) * scale;
+    var y = p[1] / Math.pow(2,16) * scale;
+    var lon = (x * 180 / scale) - 180;
+    var lat = -(y * 180 / scale - 90);
+    var lonlat = new OpenLayers.LonLat(lon, lat);
+    return lonlat;
+}
+
+function addLayer(name, path, isBaseLayer) {
+    var layer = new OpenLayers.Layer.XYZ(name, path, {numZoomLevels: 10, alpha: true, layers: 'basic'});
+    layer.setIsBaseLayer(isBaseLayer);
+    map.addLayer(layer);
 }
 
 OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
@@ -36,41 +54,39 @@ OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
         );
     }, 
 
-    onClick: function(evt) {  
-        //map.panTo(map.getLonLatFromPixel(evt.xy));
-		var latlon = map.getLonLatFromPixel(evt.xy);
-		var zoom = map.getZoom();
-		var scale = Math.pow(2, zoom - 1) * 256; // one edge length
-		// note: lon is being scaled by 180 because the IP map is square
-		// and only half of the geographic space is in use
-		var x = scale * ((latlon.lon + 180) / 180);
-		var y = scale * (1 - (latlon.lat + 90) / 180);
-		if (x >= 0 && x < scale && y >= 0 && y < scale) {
-			var s = xy2d(scale, x, y);
-			var ip = num2ip(s / (scale * scale) * Math.pow(2, 32));
-			document.getElementById('currentLocation').innerHTML = ip;
-			//alert(ip);
-		}
-    }   
+    onClick: function(evt) {
+        var latlon = map.getLonLatFromPixel(evt.xy);
+        var zoom = map.getZoom();
+        var scale = Math.pow(2, zoom - 1) * 256; // one edge length
+        // note: lon is being scaled by 180 because the IP map is square
+        // and only half of the geographic space is in use
+        var x = scale * ((latlon.lon + 180) / 180);
+        var y = scale * (1 - (latlon.lat + 90) / 180);
+        if (x >= 0 && x < scale && y >= 0 && y < scale) {
+            var s = xy2d(scale, x, y);
+            var ip = num2ip(s / (scale * scale) * Math.pow(2, 32));
+            document.getElementById('currentLocation').value = ip;
+        }
+    }
 
 });
 
 function createMap() {
-	// Limit the map to the left half since the ip map is square
-	var extent = new OpenLayers.Bounds(-180, -90, 0, 90);
+    // Limit the map to the left half since the ip map is square
+    var extent = new OpenLayers.Bounds(-180, -90, 0, 90);
     var options = {
         restrictedExtent: extent
-    }
+    };
 
     var map = new OpenLayers.Map('map', options);
-    
-	var click = new OpenLayers.Control.Click();
-	map.addControl(click);
-	click.activate();
 
-	var switcherControl = new OpenLayers.Control.LayerSwitcher();
-	map.addControl(switcherControl);
-	switcherControl.maximizeControl();
+    var click = new OpenLayers.Control.Click();
+    map.addControl(click);
+    click.activate();
 
-	return map;
+    var switcherControl = new OpenLayers.Control.LayerSwitcher();
+    map.addControl(switcherControl);
+    switcherControl.maximizeControl();
+
+    return map;
 }
